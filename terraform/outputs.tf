2393fd3788f7230e14da11e638438f18f07241d2
Abhishektondihal <110486795+Abhishektondihal@users.noreply.github.com>
2026-02-07 00:09:06 +0530

    Configuring an automated build that builds docker image and also runs terraform commands when pushed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "job_name" {
 output "scheduler_name" {
   value = google_cloud_scheduler_job.snapshot_trigger.name
 }
+
+output "wif_provider" {
+  value = google_iam_workload_identity_pool_provider.github_provider.name
+}
+
+output "terraform_sa_email" {
+  value = google_service_account.terraform_sa.email
+}
